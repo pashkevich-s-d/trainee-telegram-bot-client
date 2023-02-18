b443942c4e2f8881bc15fc0d3784ba4bfdae581e
Sergei Pashkevich <pashkevich.s.d@gmail.com>
2023-02-18 23:36:04 +0300

    send document support

--- src/Service/TelegramBotClient.php
+++ src/Service/TelegramBotClient.php
@@ -6,6 +6,8 @@ use Exception;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Mime\Part\DataPart;
+use Symfony\Component\Mime\Part\Multipart\FormDataPart;
 use PashkevichSD\TraineeTelegramBotClient\Model\Request\Message;
 use PashkevichSD\TraineeTelegramBotClient\Exception\TelegramBotClientException;
 
@@ -14,6 +16,7 @@ class TelegramBotClient
     private const API = 'https://api.telegram.org/bot';
     private const SEND_MESSAGE = 'sendMessage';
     private const GET_UPDATES = 'getUpdates';
+    private const SEND_DOCUMENT = 'sendDocument';
 
     private HttpClientInterface $httpClient;
 
@@ -26,7 +29,7 @@ class TelegramBotClient
         $this->httpClient = HttpClient::create();
     }
 
-    public function sendMessage(Message $message)
+    public function sendMessage(Message $message): array
     {
         try {
             $response = $this->httpClient->request(
@@ -45,6 +48,8 @@ class TelegramBotClient
                 $exception->getPrevious()
             );
         }
+
+        return $response->toArray();
     }
 
     public function getUpdates(): array
@@ -60,6 +65,36 @@ class TelegramBotClient
         }
 
         return json_decode($response->getContent(), true);
+    }
+
+    public function sendDocument(
+        string $chatId,
+        string $filePath
+    ): array {
+        $formFields = [
+            'chat_id' => $chatId,
+            'document' => DataPart::fromPath($filePath),
+        ];
+        $formData = new FormDataPart($formFields);
+
+        try {
+            $response = $this->httpClient->request(
+                Request::METHOD_POST,
+                $this->getMethodUrl(self::SEND_DOCUMENT),
+                [
+                    'headers' => $formData->getPreparedHeaders()->toArray(),
+                    'body' => $formData->bodyToIterable(),
+                ]
+            );
+        } catch (Exception $exception) {
+            throw new TelegramBotClientException(
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception->getPrevious()
+            );
+        }
+
+        return $response->toArray();
     }
 
     private function getMethodUrl(string $method): string
